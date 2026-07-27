50cc545b5cceb62d8e283e0fc99c411cc6ffbca5
Boopathi-dell <boopathi.mec.cse@gmail.com>
2026-07-27 23:47:14 +0530

    chore: clean up unused Student model and routes

--- testEmail.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const sendEmail = require('./utils/emailService');
-
-sendEmail('boopathi.mec.cse@gmail.com', 'Test Mail', 'Testing from Node')
-  .then(console.log)
-  .catch(console.error);
